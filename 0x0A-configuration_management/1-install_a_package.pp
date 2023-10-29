873f016c65eb8cb951b739daefdc6fbe78778c34
cassandra18 <cassandralelei018@gmail.com>
2023-10-29 13:05:43 +0000

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,14 @@
 # Install flask
 
-package { 'flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => 'pip3 install Flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => 'pip3 show Flask | grep Version | grep -q 2.1.0',
+}
+
+#install Werkzeug
+exec { 'install_werkzeug':
+  command => 'pip3 install Werkzeug==2.1.1',
+  path    => ['/usr/bin'],
+  unless  => 'pip3 show Werkzeug | grep Version | grep -q 2.1.1',
 }
